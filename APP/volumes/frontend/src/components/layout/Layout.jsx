dfe3d9ac34af540ab2b528324e9ca9574f31621b
Black-Zeus <blk.zeus@gmail.com>
2026-02-02 18:01:45 -0300

    Layout

---
+++ APP/volumes/frontend/src/components/layout/Layout.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Sidebar from './sidebar/Sidebar';
+import Header from './header/Header';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="flex h-screen bg-gray-50">
+      {/* Sidebar */}
+      <Sidebar />
+      
+      {/* Main Content Area */}
+      <div className="flex-1 flex flex-col overflow-hidden">
+        {/* Header */}
+        <Header />
+        
+        {/* Content */}
+        <main className="flex-1 overflow-y-auto p-6">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
