245301dff3f9b740c34695c17250e6e3479102d9
Zeus - Linux <blk.zeus@gmail.com>
2026-02-28 14:19:54 -0300

    iconManager Refactoring

---
+++ APP/volumes/frontend/src/components/ui/icon/categories/fileIcons.js
@@ -0,0 +1,41 @@
+// src/components/ui/icon/categories/fileIcons.js
+import {
+  FaFileLines,
+  FaRegFileLines,
+  FaFolderClosed,
+  FaFileExport,
+  FaClipboardList,
+  FaClipboardCheck,
+  FaListCheck,
+  FaRegFile,
+  FaCode,
+  FaTerminal,
+} from "react-icons/fa6";
+
+export const fileIcons = {
+  fileLines: FaFileLines,
+  regFileLines: FaRegFileLines,
+  folderClosed: FaFolderClosed,
+  export: FaFileExport,
+  clipboardList: FaClipboardList,
+  clipboardCheck: FaClipboardCheck,
+  listCheck: FaListCheck,
+  FaList: FaListCheck,
+  regFile: FaRegFile,
+  code: FaCode,
+  terminal: FaTerminal,
+  
+  // Compatibilidad
+  FaFileLines,
+  FaRegFileLines,
+  FaFolderClosed,
+  FaFileExport,
+  FaClipboardList,
+  FaClipboardCheck,
+  FaListCheck,
+  FaRegFile,
+  FaCode,
+  FaTerminal,
+  list: FaListCheck,
+   FaFileAlt: FaFileLines,
+};
---
+++ APP/volumes/frontend/src/components/ui/icon/categories/navigationIcons.js
@@ -0,0 +1,33 @@
+// src/components/ui/icon/categories/navigationIcons.js
+import {
+  FaChevronLeft,
+  FaChevronRight,
+  FaChevronDown,
+  FaChevronUp,
+  FaArrowRight,
+  FaArrowUpRightFromSquare,
+  FaHouse,
+  FaDoorOpen,
+} from "react-icons/fa6";
+
+export const navigationIcons = {
+  chevronLeft: FaChevronLeft,
+  chevronRight: FaChevronRight,
+  chevronDown: FaChevronDown,
+  chevronUp: FaChevronUp,
+  arrowRight: FaArrowRight,
+  externalLink: FaArrowUpRightFromSquare,
+  openExternal: FaArrowUpRightFromSquare,
+  home: FaHouse,
+  doorOpen: FaDoorOpen,
+  
+  // Compatibilidad
+  FaChevronLeft,
+  FaChevronRight,
+  FaChevronDown,
+  FaChevronUp,
+  FaArrowRight,
+  FaArrowUpRightFromSquare,
+  FaHouse,
+  FaDoorOpen,
+};
