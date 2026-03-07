1522888700c0ad506a657c0a15977c6412eb3918
Zeus - Linux <blk.zeus@gmail.com>
2026-03-06 23:12:20 -0300

    Adicion Iconos

--- APP/volumes/frontend/src/components/ui/icon/index.js
+++ APP/volumes/frontend/src/components/ui/icon/index.js
@@ -1,3 +1,3 @@
 // src/components/ui/icon/index.js
-export { ICON_REGISTRY, getIconCategory, getIconsByCategory } from './registry';
-export { normalizeName, warnMissingIcon } from './normalizer';
+export { ICON_REGISTRY, getIconCategory, getIconsByCategory } from './iconRegistry';
+export { normalizeName, warnMissingIcon } from './iconNormalizer';
